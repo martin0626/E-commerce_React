473656d090abc5f01c659973a865d87551301b8f
martin0626 <scbsteel12@gmail.com>
2023-03-28 21:38:53 +0300

    Finished Image Slides Section Functionality

--- src/components/Home/ImageSlides.js
+++ src/components/Home/ImageSlides.js
@@ -1,32 +1,73 @@
+import { useState } from "react";
 import classes from "./ImageSlides.module.css";
 
+let images = [
+  {
+    id: "1",
+    img: "https://i.insider.com/5d68393e2e22af2a6e5aba14?width=1136&format=jpeg",
+  },
+  {
+    id: "2",
+    img: "https://th.bing.com/th?id=OIP.JGJfbwATNdQjAZjgW9MWIgHaEo&w=316&h=197&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
+  },
+  {
+    id: "3",
+    img: "https://th.bing.com/th?id=OIP.WlTthXGmL7Ax0lYId5fCGwHaFS&w=295&h=211&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
+  },
+];
+
 const ImageSlides = () => {
+  let [currIndex, setCurrIndex] = useState(0);
+  // let [activeImage, setActiveImage] = useState(images[0].id);
+  console.log(currIndex, "Outside");
+
+  const changeIndexHandler = (step) => {
+    let index = currIndex + step;
+
+    if (index === images.length) {
+      setCurrIndex(0);
+      // setActiveImage(images[currIndex].id);
+      return;
+    }
+    if (index < 0) {
+      setCurrIndex(images.length - 1);
+      // setActiveImage(images[currIndex].id);
+      return;
+    }
+
+    setCurrIndex(index);
+    // setActiveImage(images[currIndex].id);
+  };
+
   return (
     <div className={classes["slide-container"]}>
       <div className={`${classes.fade} ${classes.slide} ${classes.active}`}>
-        <img
-          className={classes["slide-image"]}
-          src="https://i.insider.com/5d68393e2e22af2a6e5aba14?width=1136&format=jpeg"
-        />
+        <img className={classes["slide-image"]} src={images[currIndex].img} />
       </div>
-      <div className={`${classes.fade} ${classes.slide}`}>
-        <img
-          className={classes["slide-image"]}
-          src="https://newspaperads.ads2publish.com/wp-content/uploads/2018/10/reebok-clothing-your-move-is-not-mine-ad-times-of-india-mumbai-13-10-2018.png"
-        />
-      </div>
-      <div className={`${classes.fade} ${classes.slide}`}>
-        <img
-          className={classes["slide-image"]}
-          src="https://i.ytimg.com/vi/fujboh-W7Sk/maxresdefault.jpg"
-        />
-      </div>
-      <a className={classes.next}>&#10094;</a>
-      <a className={classes.prev}>&#10095;</a>
+      <a
+        className={classes.next}
+        onClick={() => {
+          changeIndexHandler(-1);
+        }}
+      >
+        &#10094;
+      </a>
+      <a
+        className={classes.prev}
+        onClick={() => {
+          changeIndexHandler(1);
+        }}
+      >
+        &#10095;
+      </a>
       <div className={classes.dots}>
-        <span className={classes.dot}></span>
-        <span className={classes.dot}></span>
-        <span className={classes.dot}></span>
+        {images.map((el) => (
+          <span
+            className={`${
+              images[currIndex].id === el.id && classes["active-dot"]
+            } ${classes.dot}`}
+          ></span>
+        ))}
       </div>
     </div>
   );
